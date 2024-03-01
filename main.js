807247f2e9f0e1434e06a1786550ab0964464505
0911bekjon <bekjonnorbotayev9@gmail.com>
2024-03-02 01:07:55 +0500

    asd

---
+++ main.js
@@ -0,0 +1,138 @@
+let a = 200
+let b = 500
+let total = a * b
+
+let filiaalone = (total / 100) * 29
+let filiaalTwo = (total / 100) * 71
+
+
+console.log("total " + total);
+console.log("filiaalone " + filiaalone);
+console.log("filiaalTwo  " + filiaalTwo);
+let c = 120
+let d = 432
+let totalOne = c / d
+
+let filiaalOne = (total / 100) * 31
+let filiaalThre = (total / 100) * 69
+
+
+console.log("total " + total);
+console.log("filiaalOne " + filiaalOne);
+console.log("bc  " + filiaalThre);
+
+let e = 935
+let f = 500
+let totalTwo = e - f
+
+let filiaalFour = (total / 100) * 12
+let filiaalFive = (total / 100) * 88
+
+
+console.log("total " + total);
+console.log("filiaalFour " + filiaalFour);
+console.log("filiaalFive  " + filiaalFive);
+let g = 200
+let h = 912
+let totalThree = g + h
+
+let filiaalSix = (total / 100) * 90
+let filiaalSeven = (total / 100) * 10
+
+
+console.log("total " + total);
+console.log("filiaalSix " + filiaalSix);
+console.log("filiaalSeven  " + filiaalSeven);
+let i = 120
+let j = 123
+let totalFour = j ** i
+
+let filiaalEight = (total / 100) * 55
+let filiaalNine = (total / 100) * 45
+
+
+console.log("total " + total);
+console.log("ab " + filiaalone);
+console.log("bc  " + filiaalTwo);
+
+
+
+let car = 41000
+let carOne = 1100
+let carTwo = 800
+let workDay = 14
+
+let totalCars = (car + carOne + carTwo) * workDay
+let totalCarsOne = car * workDay
+let totalCarsTwo = carOne * workDay
+let totalCarsThree = carTwo * workDay
+
+
+console.log(totalCars);
+console.log(totalCarsOne);
+console.log(totalCarsTwo);
+console.log(totalCarsThree);
+
+let carThree = 41000
+let carFour = 1100
+let carFive = 800
+let workDayOne = 14
+
+let totalCars1 = (carThree + carFour + carFive) * workDay
+let totalCarsOne2 = carThree * workDay
+let totalCarsTwo3 = carFour * workDay
+let totalCarsThree4 = carFive * workDay
+
+
+console.log(totalCars);
+console.log(totalCarsOne);
+console.log(totalCarsTwo);
+console.log(totalCarsThree);
+
+let carSix = 41000
+let carOneSeven = 1100
+let carTwoEight = 800
+let workDayTwo = 14
+
+let totalCars5 = (car + carOne + carTwo) * workDay
+let totalCarsOne6 = car * workDay
+let totalCarsTwo7 = carOne * workDay
+let totalCarsThree8 = carTwo * workDay
+
+
+console.log(totalCars);
+console.log(totalCarsOne);
+console.log(totalCarsTwo);
+console.log(totalCarsThree);
+
+let carNine = 41000
+let carOneTen = 1100
+let carTwoEleven = 800
+let workDayThree = 14
+
+let totalCars9 = (car + carOne + carTwo) * workDay
+let totalCarsOne10 = car * workDay
+let totalCarsTwo11 = carOne * workDay
+let totalCarsThree12 = carTwo * workDay
+
+
+console.log(totalCars);
+console.log(totalCarsOne);
+console.log(totalCarsTwo);
+console.log(totalCarsThree);
+
+let carTwelve = 41000
+let carOneThirty = 1100
+let carTwoFourten = 800
+let workDayFour = 14
+
+let totalCars13 = (car + carOne + carTwo) * workDay
+let totalCarsOne14 = car * workDay
+let totalCarsTwo15 = carOne * workDay
+let totalCarsThree16 = carTwo * workDay
+
+
+console.log(totalCars);
+console.log(totalCarsOne);
+console.log(totalCarsTwo);
+console.log(totalCarsThree);
